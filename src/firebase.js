96bbc2d9808c928ed579c359be32d3c0cf077d35
Pan Teparak <panteparak@me.com>
2018-05-09 00:43:24 +0700

    add firebase conf

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyDmicnjGkCExMoie5qH2DkdwySGwMAByhY",
+  authDomain: "dreamteamhackathon.firebaseapp.com",
+  databaseURL: "https://dreamteamhackathon.firebaseio.com",
+  projectId: "dreamteamhackathon",
+  storageBucket: "dreamteamhackathon.appspot.com",
+  messagingSenderId: "274005184416"
+};
+firebase.initializeApp(config);
+
+export default firebase;
+export const db = firebase.db;
+export const auth = firebase.auth;
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import firebase from "./firebase";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+// Can reference firebase using Vue.firebase.auth()......
+Vue.firebase = firebase;
 
 new Vue({
   router,
