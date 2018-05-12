ed022e1469e549848d062b6a609def4b5ebd4761
Pan Teparak <panteparak@me.com>
2018-05-12 17:44:04 +0700

    sunny wanted to rename

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,17 +1,17 @@
 <template>
     <v-app id="main">
         <Toolbar :drawer="drawer" v-on:toggle-drawer="setDrawer"/>
-        <NavigationDrawer :drawer="drawer" v-model="setDrawer" />
+        <SideNav :drawer="drawer" v-model="setDrawer" />
     </v-app>
 </template>
 
 <script>
 import Toolbar from "@/components/Toolbar.vue";
-import NavigationDrawer from "@/components/NavigationDrawer.vue";
+import SideNav from "@/components/SideNav.vue";
 
 export default {
   components: {
-    NavigationDrawer,
+    SideNav,
     Toolbar
   },
   data: () => ({
