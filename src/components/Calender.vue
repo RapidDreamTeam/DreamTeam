ec35517012e83ac76d1d7a36081d4a6bca2457a2
Sanchit Chawla <sanchit_sid@hotmail.com>
2018-05-12 18:24:49 +0700

    pushing calnder to switch to ubuntu

---
+++ src/components/Calender.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+    <Calendar startDate="2018-03-07" :dateData="data">
+  <div slot="header-left">
+    <Button>month</Button>
+    <Button>week</Button>
+  </div>
+
+  <div
+    :class="['calendar-item', { 'is-otherMonth': !isCurMonth }]"
+    slot-scope="item"
+  >
+    <div
+      :class="['calendar-item-date']">
+      {{item.date.date}}
+    </div>
+    <div class="calendar-item-name">{{item.data.title}}</div>
+  </div>
+</Calendar>
+
+  </v-app>
+</template>
+
+<script>
+import "vue2-event-calendar/default.css";
+import Calendar from "vue2-event-calendar";
+export default {
+  components: {
+    Calendar
+  },
+  methods: {
+    renderHeader({ prev, next, selectedDate }) {
+      const h = this.$createElement;
+
+      const prevButton = h(
+        "div",
+        {
+          on: {
+            click: prev
+          }
+        },
+        ["prev"]
+      );
+
+      const nextButton = h(
+        "div",
+        {
+          on: {
+            click: next
+          }
+        },
+        ["next"]
+      );
+
+      return h("div", [prevButton, selectedDate, nextButton]);
+    }
+  }
+};
+</script>
