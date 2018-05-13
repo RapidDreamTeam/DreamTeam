e2b16f781c36b88f2d0b011eac125942b64614bf
Pan Teparak <panteparak@me.com>
2018-05-14 01:44:45 +0700

    refactoring code

--- src/main.js
+++ src/main.js
@@ -2,20 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import firebase, {db, auth} from "./firebase";
+import firebase, { db, auth } from "./firebase";
 import Vuetify from "vuetify";
 import "./registerServiceWorker";
 import "babel-polyfill";
-// import "vuetify/dist/vuetify.min.css";
 import "vue-material-design-icons/styles.css";
 
 Vue.use(Vuetify);
 Vue.config.productionTip = false;
 
 // Can reference firebase using Vue.firebase.auth()......
-Vue.$firebase = firebase;
+Vue.firebase = firebase;
 Vue.db = db;
-Vue.auth = auth
+Vue.auth = auth;
 
 new Vue({
   router,
--- src/store.js
+++ src/store.js
@@ -9,62 +9,130 @@ Vue.use(Vuex);
 // uid/workHours
 // uid/freeHours
 
-export default new Vuex.Store({
+// Pan Was here!!
+
+// export default new Vuex.Store({
+//   state: {
+//     freeHours: {},
+//     workHours: {},
+//     lectureHours: {}
+//   },
+//   mutations: {},
+//   actions: {
+//     getFreeHours({ commit }, { uid, day }) {
+//       const { db } = Vue;
+//       console.log(db);
+
+//       const userMeta = db
+//         .ref(`${uid}/meta`)
+//         .once("value", snapshot => {
+//           const eta = {
+//             meta: snapshot.val(),
+//             id: snapshot.key
+//           };
+//           return eta.meta;
+//         })
+//         .then(meta => {
+//           this.setState({
+//             freeHours: meta.week.day.freeHours
+//           });
+//           console.log("Done with freeHours");
+//         });
+//     },
+//     getWorkHours({ commit }, { uid, day }) {
+//       const { db } = Vue;
+//       console.log(db);
+
+//       const userMeta = db
+//         .ref(`${uid}/meta`)
+//         .once("value", snapshot => {
+//           const eta = {
+//             meta: snapshot.val(),
+//             id: snapshot.key
+//           };
+//           return eta.meta;
+//         })
+//         .then(meta => {
+//           this.setState({
+//             workHours: meta.week.day.workHours
+//           });
+//           console.log("Done with workHours");
+//         });
+//     },
+//     getLectureHours({ commit }, { uid, day }) {
+//       const { db } = Vue;
+//       const hours = [];
+//       const userMeta = db
+//         .ref(`${uid}/classes`)
+//         .once("value", snapshot => {
+//           snapshot.forEach(childSnapshot => {
+//             const eachClass = {
+//               classData: childSnapshot.val(),
+//               id: childSnapshot.key
+//             };
+
+//             classData.days.forEach(lsDay => {
+//               if (lsDay == day) {
+//                 hours += classData.hours;
+//               }
+//             });
+//           });
+//           return hours;
+//         })
+//         .then(hours => {
+//           this.setState({
+//             lectureHours: hours
+//           });
+//           console.log("Done with lectureHours");
+//         });
+//     }
+//   }
+// });
+
+new Vuex.Store({
   state: {
     freeHours: {},
     workHours: {},
-    lectureHours: {}
+    lectureHours: {},
+    loading: false
   },
-  mutations: {},
+  mutations: {
+    setFreeHours: (state, payload) => {
+      state.freeHours = payload;
+    },
+    setWorkHours: (state, payload) => {
+      state.workHours = payload;
+    },
+    setLectureHours: (state, payload) => {
+      state.lectureHours = payload;
+    },
+    setLoading: (state, payload) => {
+      state.loading = payload;
+    }
+  },
   actions: {
-    getFreeHours({ commit }, {uid, day}) {
-      const {db} = Vue
-      console.log(db);
-
-      const userMeta = db.ref(`${uid}/meta`).once('value', snapshot => {
-      const eta = {meta: snapshot.val(), id: snapshot.key};
-      return eta.meta;
-    }).then((meta) => {
-      this.setState({
-        freeHours : meta.week.day.freeHours
-      });
-      console.log("Done with freeHours");
-    });
-  },
-  getWorkHours({ commit }, {uid, day}){
-    const {db} = Vue
-    console.log(db);
-
-    const userMeta = db.ref(`${uid}/meta`).once('value', snapshot => {
-    const eta = {meta: snapshot.val(), id: snapshot.key};
-    return eta.meta;
-  }).then((meta) => {
-      this.setState({
-        workHours : meta.week.day.workHours
-      });
-      console.log("Done with workHours");
-        });
-      },
-    getLectureHours({ commit }, {uid, day}){
-      const {db} = Vue
-      const hours = []
-      const userMeta = db.ref(`${uid}/classes`).once('value', snapshot => {
-        snapshot.forEach(childSnapshot => {
-          const eachClass = {classData: childSnapshot.val(), id: childSnapshot.key};
-
-          classData.days.forEach(lsDay => {
-            if (lsDay == day) {
-              hours+=classData.hours;
-            }
-          });
-        })
-        return hours;
-    }).then((hours) => {
-        this.setState({
-          lectureHours : hours
-        });
-        console.log("Done with lectureHours");
-          });
-        }
+    computeFreeHours: ({ commit }, { uid, day }) => {
+      // const { db } = Vue;
+      // db
+      //   .ref(`${uid}/meta`)
+      //   .once("value", ({ val, key }) => ({
+      //     meta: val(),
+      //     id: key
+      //   }))
+      //   .then(payload => commit("setFreeHours", payload));
+    },
+    getWorkHours: ({ commit }, { uid, day }) => {
+      // const { db } = Vue;
+      // db
+      //   .ref(`${uid}/meta`)
+      //   .once("value", ({ val, key }) => ({
+      //     meta: val(),
+      //     id: key
+      //   }))
+      //   .then(meta => commit("setWorkHours", meta.week.day.workHours));
+    },
+    getLectureHours: ({ commit }, { uid, day }) => {
+      // commit("setLectureHours", null);
+    }
   }
 });
