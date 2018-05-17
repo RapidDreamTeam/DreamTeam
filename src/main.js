9c4a0b09e229b60c1a700189c964c73d4f9fecfe
Pan Teparak <panteparak@me.com>
2018-05-17 14:09:19 +0700

    login

--- src/main.js
+++ src/main.js
@@ -26,10 +26,16 @@ const unsubscribe = firebase.auth().onAuthStateChanged(firebaseUser => {
     beforeRouteEnter(to, from, next) {
       if (["/"].includes(this.$router.path)) {
         // store.dispatch("signoutWithoutRedirect")
+
+        console.log(firebaseUser.user);
+
+        if (firebaseUser) {
+          store.dispatch("currentUser", firebaseUser.user);
+        }
         next("/");
       } else if (firebaseUser) {
         store
-          .dispatch("currentUser", firebaseUser)
+          .dispatch("currentUser", firebaseUser.user)
           .then(() => next("/dashboard"));
       } else {
         store.dispatch("signout", null);
